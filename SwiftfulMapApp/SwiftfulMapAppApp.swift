510172c294a0a217ae1912a95122045eb27ac181
Darrell Stone <darrellstone3@gmail.com>
2025-02-27 15:08:51 -0500

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Darrell Stone on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulMapAppTests/SwiftfulMapAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppTests.swift
+//  SwiftfulMapAppTests
+//
+//  Created by Darrell Stone on 2/27/25.
+//
+
+import Testing
+@testable import SwiftfulMapApp
+
+struct SwiftfulMapAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
